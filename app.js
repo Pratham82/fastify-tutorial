d7c8a8eaddd1606a9083f16d8eea6cd14a6e6958
pratham82 <mali.prathamesh82@gmail.com>
2021-07-06 15:26:21 +0530

    regsitered swagger endpoint

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-const fastify = require('fastify')({logger:true})
-const connectToDB  = require('./utils/db-connection.js')
-require('dotenv').config()
+const fastify = require("fastify")({ logger: true });
+const connectToDB = require("./utils/db-connection.js");
+require("dotenv").config();
 
 // DB connection
-connectToDB()
+connectToDB();
+
+//Registering swagger
+fastify.register(require("fastify-swagger"), {
+  exposeRoute: true,
+  routePrefix: "/docs",
+  swagger: {
+    info: { title: "fastify-crud" },
+  },
+});
 
 // Registering routes
-fastify.register(require('./routes/users.js'))
+fastify.register(require("./routes/users.js"));
 
-// Routes
-fastify.get('/test', async (_,rep) =>{
+// Starting the server with iffie
+(async function main() {
   try {
-   await rep.send('Hello World')
+    await fastify.listen(process.env.PORT || 5000, () =>
+      console.log(`Server Running on PORT ${process.env.PORT} ✅`)
+    );
   } catch (error) {
-    fastify.log.error(error)
+    fastify.log.error(error);
+    process.exit(1);
   }
-})
+})();
 
 // Conventional starter
-// const start = async () =>{ 
+// const start = async () =>{
 //   try {
-//    await fastify.listen(process.env.PORT, () => console.log(`Server Running on ${process.env.PORT} ✅`)) 
+//    await fastify.listen(process.env.PORT, () => console.log(`Server Running on ${process.env.PORT} ✅`))
 //   } catch (error) {
 //     fastify.log.error(error)
 //     process.exit(1)
 //   }
 // }
-// 
+//
 // start()
-
-// Starting the server with iffie
-;(async function main(){ 
-  try {
-   await fastify.listen(process.env.PORT, () => console.log(`Server Running on ${process.env.PORT} ✅`)) 
-  } catch (error) {
-    fastify.log.error(error)
-    process.exit(1)
-  }
-})()
-
