6ddacb9ef3bee6db9d028d9b5d92fa4a11c2d992
pratham82 <mali.prathamesh82@gmail.com>
2021-07-06 15:24:47 +0530

    Added database related scripts

---
+++ scripts/load_users.sh
@@ -0,0 +1,2 @@
+# !/bin/bash
+mongo < mongo_script.js
---
+++ scripts/mongo_script.js
@@ -0,0 +1,53 @@
+use users_db;
+db.users.insertMany(
+[
+{
+    "name":"Roxy",
+    "email":"roxy@gmail.com",
+    "phone": 1234567890,
+    "city":"Atlanta",
+    "state": "Georgia",
+    "area":"Brookhaven",
+    "country":"IN"
+},
+
+{
+    "name":"Drake",
+    "email":"drake@gmail.com",
+    "phone": 7778889991,
+    "city":"Buffalo",
+    "state": "New York",
+    "area":"Allentown",
+    "country":"US"
+},
+
+{
+    "name":"Justin",
+    "email":"justin@gmail.com",
+    "phone": 5558884121,
+    "city":"Lost Angeles",
+    "state": "California",
+    "area":"Santa Monica",
+    "country":"US"
+},
+
+{
+    "name":"Ron",
+    "email":"ron@gmail.com",
+    "phone": 7885449552,
+    "city":"Chicago",
+    "state": "Illinois",
+    "area":"Andersonville",
+    "country":"US"
+},
+{
+    "name":"John",
+    "email":"john@gmail.com",
+    "phone": 1234567890,
+    "city":"Honolulu",
+    "state": "Hawai",
+    "area":"Waikiki",
+    "country":"IN"
+},
+]
+)
